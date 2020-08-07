59dae734cceed3fe4f22b6a9bc7f7ea0e6e1c455
Scott Haleen <scott.haleen@gmail.com>
2020-08-07 13:41:57 -0400

    docker build stuff

--- build-docker.sh
+++ build-docker.sh
@@ -15,9 +15,15 @@ fi
 
 DT=$(date +"%Y%m%d")
 GIT=${DT}.git.$(git rev-parse --short HEAD)
-VERSION="0.1"
+VERSION="0.1.1"
 GROUP=jataware
 NAME=vpnproxy
 IMAGE="${GROUP}/${NAME}"
 
-docker build -t "${IMAGE}:dev" -t "${IMAGE}:${VERSION}" -t "${IMAGE}:${GIT}" -t "${NAME}:${VERSION}" .
+docker build \
+       -t "${IMAGE}:dev" \
+       -t "${IMAGE}:${VERSION}" \
+       -t "${IMAGE}:${GIT}" \
+       -t "${NAME}:${VERSION}" \
+       -t "registry.gitlab.com/${IMAGE}:${VERSION}" \
+       .
