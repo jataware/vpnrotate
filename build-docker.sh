ae15a97733eeb6d168d5d522cf7e18efb1a5a578
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2021-04-07 17:05:28 -0700

    added wind cert/key copy

--- build-docker.sh
+++ build-docker.sh
@@ -11,10 +11,12 @@ if [ ! -d "nordvpn" ]; then
   wget -q -O pia/pia_tmp.zip https://www.privateinternetaccess.com/openvpn/openvpn.zip
   unzip -q pia/pia_tmp.zip -d pia
   cp pia/* nordvpn/ovpn_tcp/
-
+  
   mkdir wind
   #wget -q -O pia/pia_tmp.zip https://NONE.FOUND.YET
+  cp cert_key/* wind/
   cp wind_tmp.zip wind/wind_tmp.zip
+  unzip -q wind/openvpn_cert.zip -d wind
   unzip -q wind/wind_tmp.zip -d wind
   rm wind/wind_tmp.zip
   cp wind/* nordvpn/ovpn_tcp/
