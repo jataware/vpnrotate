4ce08cdbc1e6a566fb942bee989d825c2490cb0b
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2021-04-09 10:19:04 -0700

    aws and added new docker build

--- build-docker.sh
+++ build-docker.sh
@@ -1,30 +1,8 @@
 #!/usr/bin/env sh
 
-
-if [ ! -d "nordvpn" ]; then
-  mkdir nordvpn
-  wget -q -O nordvpn/ovpn_tmp.zip https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
-  unzip -q nordvpn/ovpn_tmp.zip -d nordvpn
-  rm nordvpn/ovpn_tmp.zip
-
-  mkdir pia
-  wget -q -O pia/pia_tmp.zip https://www.privateinternetaccess.com/openvpn/openvpn.zip
-  unzip -q pia/pia_tmp.zip -d pia
-  cp pia/* nordvpn/ovpn_tcp/
-  
-  mkdir wind
-  #wget -q -O pia/pia_tmp.zip https://NONE.FOUND.YET
-  cp cert_key/* wind/
-  cp wind_tmp.zip wind/wind_tmp.zip
-  unzip -q wind/openvpn_cert.zip -d wind
-  unzip -q wind/wind_tmp.zip -d wind
-  rm wind/wind_tmp.zip
-  cp wind/* nordvpn/ovpn_tcp/
-fi
-
 DT=$(date +"%Y%m%d")
 GIT=${DT}.git.$(git rev-parse --short HEAD)
-VERSION="0.1.1"
+VERSION="0.1.3"
 GROUP=jataware
 NAME=vpnproxy
 IMAGE="${GROUP}/${NAME}"
