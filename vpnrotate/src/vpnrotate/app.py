405da33dd43b5a05b16b74dd43b82625e02bbf4f
travis@jataware.com <travis@jataware.com>
2021-04-19 11:12:05 -0700

    ran tox -e format

--- vpnrotate/src/vpnrotate/app.py
+++ vpnrotate/src/vpnrotate/app.py
@@ -2,8 +2,8 @@ import logging
 from logging import Logger
 
 # Comment out for TOX test
-# from aiohttp import web
-# from aiohttp_swagger3 import SwaggerDocs, SwaggerUiSettings
+from aiohttp import web
+from aiohttp_swagger3 import SwaggerDocs, SwaggerUiSettings
 
 from . import __version__, config, handler, metrics
 
