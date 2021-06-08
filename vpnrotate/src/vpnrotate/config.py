7782887e9c31858d8fcc7f2a2eeb5e3cb075d02a
Scott Haleen <scott.haleen@gmail.com>
2021-06-08 16:55:18 -0400

    fix

--- vpnrotate/src/vpnrotate/config.py
+++ vpnrotate/src/vpnrotate/config.py
@@ -93,8 +93,10 @@ def get_config() -> Any:
         options = parser.parse_args()
         settings = load_settings(f"{options.resources}/{options.config}")
 
-        if ovnp_download_on_start := os.getenv("OVPN_DOWNLOAD_ON_START"):
-            settings["vpn_env"]["reload_configs_on_startup"] = yaml.safe_load(ovpn_download_on_start) == True
+        if ovpn_download_on_start := os.getenv("OVPN_DOWNLOAD_ON_START"):
+            settings["vpn_env"]["reload_configs_on_startup"] = (
+                True if yaml.safe_load(ovpn_download_on_start) is True else False
+            )
 
         app_config.check(settings)
         setup_logging(f"{options.resources}/{options.logging}")
