31eb6d8425e7436b78aeefc226d874029829c4f3
Scott Haleen <scott.haleen@gmail.com>
2022-02-25 17:11:59 -0500

    patch bump

--- build-docker.sh
+++ build-docker.sh
@@ -3,7 +3,7 @@
 DT=$(date +"%Y%m%d")
 GIT=${DT}.git.$(git rev-parse --short HEAD)
 
-VERSION="0.2.2"
+VERSION="0.2.3"
 
 GROUP=jataware
 NAME=vpnproxy
--- vpnrotate/setup.py
+++ vpnrotate/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 version managed by bump2version
 """
 
-VERSION = "0.2.2"
+VERSION = "0.2.3"
 
 
 def read_requirements(path: str):
